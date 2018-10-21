3c4af9962eb5db303f303e140be32ae2d316f7fd
Nicolas <nico.anabalon7@gmail.com>
2018-10-21 20:13:09 -0300

    Formulario

--- nixSitioWeb/src/app/perfil/perfil.component.ts
+++ nixSitioWeb/src/app/perfil/perfil.component.ts
@@ -1,20 +1,28 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
-
+import { FormBuilder, FormGroup} from '@angular/forms';
 import { User } from '../_models';
 import { UserService } from '../_services';
 
 @Component({templateUrl: 'perfil.component.html'})
 export class PerfilComponent implements OnInit {
+    registerForm: FormGroup;
     currentUser: User;
     users: any;
 
-    constructor(private userService: UserService) {
+    constructor(private userService: UserService ,private formBuilder: FormBuilder,) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
 
     ngOnInit() {
         this.loadAllUsers();
+         this.registerForm = this.formBuilder.group({
+            modificarNombre: [''] });
+    }
+
+    modificarNombre(nombre: string){
+        console.log(this.registerForm)
+        
     }
 
     deleteUser(id: number) {
@@ -27,6 +35,9 @@ export class PerfilComponent implements OnInit {
         this.userService.getAll().pipe(first()).subscribe(users => { 
             this.users = users; 
         });
+    }
+    onSubmit() {
+        
     }
     
 }
